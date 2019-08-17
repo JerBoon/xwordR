218b161e586cb47e78b2f0d97fb7be70ff145ec2
Jer Boon <jer.booner@gmail.com>
2019-08-17 10:52:56 +0100

    init_puzzle() - Initialise a fully connected puzzle

--- R/fill_puzzle.R
+++ R/fill_puzzle.R
@@ -10,12 +10,16 @@
 #' @examples
 fill_puzzle <- function(puz,random=F) {
 
-  puz <- dplyr::arrange(puz,desc(Length))
+  puz <- dplyr::arrange(puz,desc(Length),No)
 
   for (i in 1:nrow(puz)) {
     #print(paste(puz[i,]$x,puz[i,]$y,puz[i,]$DA,puz[i,]$Length,random,sep=","))
     #print_puzzle(puz)
     puz[i,]$Word <- pick_a_word(puzzle_to_grid(puz),puz[i,]$x,puz[i,]$y,puz[i,]$DA,puz[i,]$Length,random=random)
   }
+
+  if (sum(is.na(puz$Word)) > 0)
+    message(paste(" *Failed to fill",sum(is.na(puz$Word)),"words"))
+
   return(puz)
 }
